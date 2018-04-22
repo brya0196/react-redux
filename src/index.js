db64283734217938222176c7ef2ac4b5eaa8b3d0
Bryan Astacio <brya0196@gmail.com>
2018-04-22 19:37:05 -0400

    added the logic to make http request with axios and redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import store from './store';
-
 import { Provider } from 'react-redux';
+
+import { loadProducts } from "./actionCreators";
+
+store.dispatch(loadProducts());
 
 ReactDOM.render(
   <Provider store={store}>
