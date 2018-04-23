4d51630517452b8af0224c6d6ee8b491e9950808
Bryan Astacio <brya0196@gmail.com>
2018-04-22 20:47:49 -0400

    added enzyme to make tasting

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
+import * as enzyme from "enzyme";
+var Adapter = require('enzyme-adapter-react-15');
+
+enzyme.configure({ adapter: new Adapter() })
+
+
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render, shallow } from "enzyme";
+import { Provider } from "react-redux";
+import store from "./store";
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
